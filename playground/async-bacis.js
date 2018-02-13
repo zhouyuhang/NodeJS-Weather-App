bf78acb72c782be14767fcc1a1a4ce506918838c
Alex Zhou <zhouyuhangcn@hotmail.com>
2018-02-13 09:05:49 -0800

    Initial commit

---
+++ playground/async-bacis.js
@@ -0,0 +1,8 @@
+console.log('Starting app');
+setTimeout(()=>{
+  console.log("first time out");
+}, 2000);
+setTimeout(()=>{
+  console.log("second time out");
+}, 0);
+console.log('Finishing app');
---
+++ playground/callbacks.js
@@ -0,0 +1,13 @@
+var getUser = (id, callback) => {
+  var user = {
+    id:id,
+    name: 'Alex'
+  };
+  setTimeout(() => {
+    callback(user)},
+    3000);
+};
+
+getUser(31, (userObject) => {
+  console.log(userOjbect);
+});
